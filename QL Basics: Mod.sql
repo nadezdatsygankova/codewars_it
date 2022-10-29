7b1742a2571e18f6045527cced13c83b44f4cf83
nadezdatsygankova <nadiapritchina@gamil.com>
2022-10-29 10:15:24 -0400

    mod.sql

---
+++ QL Basics: Mod.sql
@@ -0,0 +1,10 @@
+/*Given the following table 'decimals':
+
+** decimals table schema **
+
+id
+number1
+number2
+Return a table with one column (mod) which is the output of number1 modulus number2.
+*/
+select MOD(number1,number2)  from decimals;
